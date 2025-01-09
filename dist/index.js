486374bdc3f97f16e29dc40ad74852f0c43cbd55
Favour Lawrence <favourlawrence234@gmail.com>
2025-01-09 13:31:41 +0100

    repo updated

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 4000;
 const MONGO_URI = process.env.MONGO_URI || '';
 app.use((0, cors_1.default)({
     credentials: true,
-    origin: ['http://localhost:3000/', 'https://tradephere.onrender.com/',]
+    origin: ['http://localhost:3000', 'https://tradephere.onrender.com']
+}));
+app.options('*', (0, cors_1.default)({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://tradephere.onrender.com']
 }));
 app.use((0, cookie_parser_1.default)());
 app.use(body_parser_1.default.json());
