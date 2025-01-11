d4940f70a5c5b1c06e5f3a21044c9ca16743dfa2
otobong23 <113153105+otobong23@users.noreply.github.com>
2025-01-11 10:19:30 +0100

    Update validator.ts

--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -7,7 +7,8 @@ export const signupSchema = Joi.object({
   // email: Joi.string().min(6).max(60).required().email({
   //   tlds: { allow:[ 'com', 'net' ] }
   // }),
-  password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+  //password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+  password: Joi.string().required().min(6)
 })
 
 export const signinSchema = Joi.object({
