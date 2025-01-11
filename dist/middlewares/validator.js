fc783bff2c711a6e1020a39627beffff54cf1a2a
Favour Lawrence <favourlawrence234@gmail.com>
2025-01-11 11:36:14 +0100

    repo updated

--- dist/middlewares/validator.js
+++ dist/middlewares/validator.js
@@ -5,28 +5,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cryptoLabelSchema = exports.watchlistSchema = exports.changePasswordSchema = exports.acceptCodeSchema = exports.signinSchema = exports.signupSchema = void 0;
 const joi_1 = __importDefault(require("joi"));
+// email: Joi.string().min(6).max(60).required().email({
+//   tlds: { allow:[ 'com', 'net' ] }
+// }),
+//password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
 exports.signupSchema = joi_1.default.object({
     firstName: joi_1.default.string().min(2).max(20).required(),
     lastName: joi_1.default.string().min(2).max(20).required(),
     username: joi_1.default.string().min(6).max(60).required(),
-    // email: Joi.string().min(6).max(60).required().email({
-    //   tlds: { allow:[ 'com', 'net' ] }
-    // }),
-    password: joi_1.default.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+    password: joi_1.default.string().required().min(6)
 });
 exports.signinSchema = joi_1.default.object({
-    // email: Joi.string().min(6).max(60).required().email({
-    //   tlds: { allow:[ 'com', 'net' ] }
-    // }),
     username: joi_1.default.string().min(6).max(6).required(),
-    password: joi_1.default.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+    password: joi_1.default.string().required().min(6)
 });
 exports.acceptCodeSchema = joi_1.default.object({
     providedCode: joi_1.default.number().required()
 });
 exports.changePasswordSchema = joi_1.default.object({
-    newPassword: joi_1.default.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$')),
-    oldPassword: joi_1.default.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+    newPassword: joi_1.default.string().required().min(6),
+    oldPassword: joi_1.default.string().required().min(6)
 });
 exports.watchlistSchema = joi_1.default.object({
     watchlist: joi_1.default.alternatives().try(joi_1.default.array().items(joi_1.default.string()).required(), joi_1.default.string().required())
--- src/middlewares/validator.ts
+++ src/middlewares/validator.ts
@@ -1,22 +1,19 @@
 import Joi from 'joi'
 
+// email: Joi.string().min(6).max(60).required().email({
+//   tlds: { allow:[ 'com', 'net' ] }
+// }),
+//password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
+
 export const signupSchema = Joi.object({
   firstName: Joi.string().min(2).max(20).required(),
   lastName: Joi.string().min(2).max(20).required(),
   username: Joi.string().min(6).max(60).required(),
-  // email: Joi.string().min(6).max(60).required().email({
-  //   tlds: { allow:[ 'com', 'net' ] }
-  // }),
-  //password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
   password: Joi.string().required().min(6)
 })
 
 export const signinSchema = Joi.object({
-  // email: Joi.string().min(6).max(60).required().email({
-  //   tlds: { allow:[ 'com', 'net' ] }
-  // }),
   username: Joi.string().min(6).max(6).required(),
-  //password: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
   password: Joi.string().required().min(6)
 })
 
@@ -25,8 +22,6 @@ export const acceptCodeSchema = Joi.object({
 })
 
 export const changePasswordSchema = Joi.object({
-  //newPassword: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$')),
-  //oldPassword: Joi.string().required().pattern(new RegExp('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$'))
   newPassword: Joi.string().required().min(6),
   oldPassword: Joi.string().required().min(6)
 })
